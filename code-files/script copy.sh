a098c9542abfe83033102cc93ccb168ef3851026
Kushal Mangal <kushal.mangal@sdettech.com>
2025-03-31 12:26:35 +0530

    fix minor issues

--- code-files/script copy.sh
+++ code-files/script copy.sh
@@ -8,11 +8,8 @@ INPUT_KEYS=$4
 # Create temporary yaml file
 cp $1 $YAML_FILE
 
-# Remove spaces from YAML file (leading & trailing)
+# Remove spaces from yaml file from leading and trailing
 sed -i 's/^[[:space:]]*//;s/[[:space:]]*$//' $YAML_FILE
-
-# Declare an associative array
-declare -A yaml_data
 
 configure_key_values(){
   key=$1
@@ -22,37 +19,35 @@ configure_key_values(){
   if [ -z "$value" ]; then
     return 0
   fi
-
-  # Apply key formatting
+  # Print the key and its corresponding value
   if [ "$KEY_FORMAT" == "uppercase" ]; then
     key=$(echo "$key" | tr '[:lower:]' '[:upper:]')
   elif [ "$KEY_FORMAT" == "lowercase" ]; then
     key=$(echo "$key" | tr '[:upper:]' '[:lower:]')
   fi
 
-  # Store key-value in an associative array
-  yaml_data["$key"]="$value"
+  # Store the key-value in a dynamically named variable
+  declare "$key=$value"
 
-  # Set GitHub environment variable
   if [ "$SET_GIT_ENV" = true ]; then
-    echo "$key=$value" >> $GITHUB_ENV
+    echo "$key=${!key}"  >> $GITHUB_ENV
   fi
-
-  # Print output
-  echo "$key=$value"
+  echo "$key=${!key}"
 }
 
 if [ "$INPUT_KEYS" == "all" ]; then
+  # Read all keys from the YAML file
   keys=$(yq e 'keys | .[]' "$YAML_FILE")
   for input in $keys; do
-    configure_key_values "$input"
+    configure_key_values $input
   done
 else
+  # Read comma seperated keys from the YAML file
   IFS=',' read -r -a INPUT_KEYS_ARRAY <<< "$INPUT_KEYS"
   for input in "${INPUT_KEYS_ARRAY[@]}"; do
-    configure_key_values "$input"
+    configure_key_values $input
   done
 fi
 
-# Cleanup 
+#clean up 
 rm -f $YAML_FILE
